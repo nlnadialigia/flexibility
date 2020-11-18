939fa7a772114421b6986231aac9dd57c0777b1f
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-18 00:20:48 -0300

    Start Landing Page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './page';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <LandingPage />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
---
+++ src/page/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const LandingPage: React.FC = () => {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+};
+
+export default LandingPage;
